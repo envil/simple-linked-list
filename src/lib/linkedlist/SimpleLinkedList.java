f0f820a46e791fe3f547d3765ea61ebe07f3e2e9
Viet Quoc Ta <viet@your.rentals>
2017-08-05 00:02:30 +0700

    Finsihed: -Delete at tail -Delete with value bigger than a specific value

--- src/lib/linkedlist/SimpleLinkedList.java
+++ src/lib/linkedlist/SimpleLinkedList.java
@@ -2,7 +2,7 @@ package lib.linkedlist;
 
 public class SimpleLinkedList {
 	/**
-	 * @category Constructor
+	 * Constructor
 	 */
 	public SimpleLinkedList() {
 		this.head = null;
@@ -13,6 +13,7 @@ public class SimpleLinkedList {
 	/**
 	 * @category Class variables
 	 */
+	
 	protected Node head;
 	protected Node tail;
 	int size;
@@ -20,6 +21,7 @@ public class SimpleLinkedList {
 	/**
 	 * @category Linked list methods
 	 */
+	
 	/**
 	 * Insert a node at the beginning of the list
 	 * @param value
@@ -39,8 +41,82 @@ public class SimpleLinkedList {
 	 */
 	public void append(int value) {
 		Node node = new Node(value);
-		tail.setLink(node);
+		if (tail == null) {
+			head = node;
+		} else {
+			tail.setLink(node);
+		}
 		tail = node;
+		++size;
+	}
+	
+	/**
+	 * Delete at specific position
+	 * @param posToDelete
+	 */
+	public void deleteAtPosition(int posToDelete) {
+		if (size == 0) {
+			System.out.print("Cannot delete since the list is empty!");
+			return;
+		}
+		if (posToDelete < 1 || posToDelete > size) {
+			System.out.println("Invalid position to delete");
+			return;
+		}
+		int currentPos = 1;
+		Node currentNode = head;
+		Node previousNode = null;
+		while (currentPos < posToDelete) {
+			previousNode = currentNode;
+			currentNode = currentNode.getLink();
+			++currentPos;
+		}
+		// delete at head
+		if (previousNode == null) {
+			head = currentNode.getLink();
+		} else {
+			previousNode.setLink(currentNode.getLink());
+		}
+		if (currentPos == size) {
+			tail = currentNode;
+		}
+		--size;
+	}
+	
+	/**
+	 * Delete the tail
+	 */
+	public void deleteTail() {
+		deleteAtPosition(size);
+	}
+	
+	/**
+	 * Delete all elements which have value greater than a value
+	 * @param value
+	 */
+	public void deleteNodeBiggerThan(int value) {
+		if (size == 0) {
+			System.out.print("Cannot delete since the list is empty!");
+			return;
+		}
+		Node currentNode = head;
+		Node previousNode = null;
+		while (currentNode != null) {
+			if (currentNode.getValue() > value) {
+				if (previousNode == null) {
+					head = currentNode.getLink();
+				} else {
+					previousNode.setLink(currentNode.getLink());
+				}
+				if (currentNode.getLink() == null) {
+					tail = previousNode;
+				}
+				--size;
+			} else {
+				previousNode = currentNode;
+			}
+			currentNode = currentNode.getLink();
+		}
 	}
 	
 	/**
@@ -66,6 +142,7 @@ public class SimpleLinkedList {
 	/**
 	 * @category Getters & Setters
 	 */
+	
 	public Node getStart() {
 		return head;
 	}
